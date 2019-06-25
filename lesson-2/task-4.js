f5ca315d2361f7ac94a205f4eed98c5382f9ed8e
juricon <yuriyvladim@gmail.com>
2019-06-25 18:29:18 +0300

    Create task-4.js

---
+++ lesson-2/task-4.js
@@ -0,0 +1,14 @@
+const value = 0;	
+switch (value){
+	case 0:
+		console.log(0);
+		break;
+	case 1:
+		console.log(1);
+		break;
+	case 2 || 3:
+		console.log('2,3');	
+		break;
+	default: 
+		alert('JS will save the World');
+}
