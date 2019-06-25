e4f6af32c800a842939d43dbb93a9a7bf2885287
juricon <yuriyvladim@gmail.com>
2019-06-25 18:31:26 +0300

    Update task-3.js

--- lesson-2/task-3.js
+++ lesson-2/task-3.js
@@ -1,3 +1,25 @@
+
+// Перепишите код, заменив оператор `switch` на оператор `if..else`:
+
+// ```js
+// const value = 'c';
+
+// switch (value) {
+//     case 'a':
+//         console.log('a');
+//         break;
+
+//     case 'b':
+//     case 'c':
+//     case 'd':
+//     case 'e':
+//         console.log('others');
+//         break;
+
+//     default:
+//         console.log('unknown');
+// }
+// `
 let value = 'c';
 if (value = 'a'){
 		console.log('others');
