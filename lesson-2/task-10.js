b8f1c1035860525e4511db7722b582fa0db183d9
juricon <yuriyvladim@gmail.com>
2019-06-27 01:31:02 +0300

    Update task-10.js

--- lesson-2/task-10.js
+++ lesson-2/task-10.js
@@ -11,3 +11,20 @@ const arr = [6, 5, 4, 3, 2, 1];
 arr.sort((a, b) => a - b);
 console.log(arr);
 	
+// by teacher
+	
+//  const array = [1, 2, 3, 4, 10, 6, 7, 8, 9, 5];
+
+// for (let i = 0; i < 9; i++) {
+//    let min = array[i];
+
+//     for (let j = i + 1; j < 10; j++) {
+//         if (array[j] < min) {
+//             const temp = array[i];
+//             min = array[j];
+//             array[i] = min;
+//             array[j] = temp;
+//         }
+//     }
+// }
+// console.log(array);
