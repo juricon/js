e39c8485612399c73a924b6bc2b64bd04c80d7fd
juricon <yuriyvladim@gmail.com>
2019-06-30 23:46:05 +0300

    Update task-2.js

--- lesson-3/task-2.js
+++ lesson-3/task-2.js
@@ -10,10 +10,25 @@
 
 // Решение
 
-/* не удалять */
-f(1, 2, 3); // 6
-f(1, 1, 1, 1, 1, 1, 1, 1); // 8
-f(1, 2, 's', 4); // Error: all parameters should be a Number type
+function f() {
+    var s = 0;
+    for (var i=0; i < arguments.length; i++) {
+        s += arguments[i];
+        if (isNaN (s) ){
+            return 'Error:all parameters should be a Number type';
+           } 
+    }
+    return s;
+   
+}
+console.log(f(1, 2, 3)); 
+console.log(f(1, 1, 1, 1, 1, 1, 1, 1)); 
+console.log(f(1, 2, 's', 4)); 
 
-export { f };
+/* не удалять */
+// f(1, 2, 3); // 6
+// f(1, 1, 1, 1, 1, 1, 1, 1); // 8
+// f(1, 2, 's', 4); // Error: all parameters should be a Number type
+
+// export { f };
 /* не удалять */
