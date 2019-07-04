9ec6ed7bf56d3a091db389bb1b8af98aea877e59
juricon <yuriyvladim@gmail.com>
2019-07-04 16:08:08 +0300

    Update task-2.js

--- lesson-4/task-2.js
+++ lesson-4/task-2.js
@@ -11,12 +11,65 @@
 
 // Решение
 
-const person = {
-    salary: 
-}
+const person = {};
+Object.defineProperties(person, {
+    rate:{
+        writable: true,   
+        enumerable: true  
+    },
+    salary:{
+        configurable: true, // there is no suck line in teacher's solution
+        get(){
+            date = new Date();
+            todayDate = date.getDate();
+            if (this.rate !== undefined){
+                return this.rate * todayDate;
+            } else {
+                return 0;
+            }
+        },
+    },
+});
+    person.rate = 10;
+    console.log(person.salary);
+
 // person.rate = 30;
 
 // // Предположим что сегодня 10 января, в этом случае это свойство возвращает число 300
 // person.salary;
 
 // exports.person = person;
+
+//teacher solution
+
+// Власник
+// Dima Vakatsiienko
+// Змінено
+// 26 черв. 2019 р. (Dima Vakatsiienko)
+// Відкрито
+// 14:47 (мною)
+// Створено
+// 26 черв. 2019 р.
+// Без опису
+// Користувачі з правами перегляду можуть завантажувати файл
+'use strict';
+
+const person = {};
+
+Object.defineProperties(person, {
+    rate: {
+        value: 0,
+        writable: true,
+    },
+    salary: {
+        get() {
+            const currentDay = new Date().getDate();
+
+            return this.rate * currentDay;
+        },
+    },
+});
+
+person.rate = 10;
+
+console.log(person.salary);
