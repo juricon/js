5c0953675fa9e81b4fc04cca2424dff4849661d5
juricon <yuriyvladim@gmail.com>
2019-07-04 12:02:58 +0300

    Update task-1.js

--- lesson-4/task-1.js
+++ lesson-4/task-1.js
@@ -1,14 +1,23 @@
-* Задача 1.
- *
- * Создайте объект `person` у которого будет одно свойство `salary`.
- * При чтении этого свойства должна возвращаться строка с текстом.
- * Если до конца месяца осталось больше чем 20 дней — возвращается строка `good salary`, а если нет — `bad salary`
- */
-
+// * Задача 1.
+//  *
+//  * Создайте объект `person` у которого будет одно свойство `salary`.
+//  * При чтении этого свойства должна возвращаться строка с текстом.
+//  * Если до конца месяца осталось больше чем 20 дней — возвращается строка `good salary`, а если нет — `bad salary`
+//  */
 const person = {};
+Object.defineProperty(person, 'salary', {
+get(){
+      var d = new Date();
+      if((d.getDate() > 20)){
+         return 'good salary';
+      } else if((d.getDate() < 20)){
+         return 'bad salary';
+      }
+   }
+});   
+ console.log(person.salary);
+ // Решение
 
-// Решение
+// person.salary; // good salary
 
-person.salary; // good salary
-
-exports.person = person;
+// exports.person = person;
