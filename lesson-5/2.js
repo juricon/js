b08302c7acfd7c7cc862a2eb4dc0d805af10b753
juricon <yuriyvladim@gmail.com>
2019-07-10 03:23:49 +0300

    Update 2.js

--- lesson-5/2.js
+++ lesson-5/2.js
@@ -12,5 +12,28 @@
 // Решение
 
 // checkSpam('pitterXXX@gmail.com', 'xxx'); // true
-// checkSpam('pitterxxx@gmail.com', 'XXX'); // true
+// chcekSpam('pitterxxx@gmail.com', 'XXX'); // true
 
+// let source;
+// let example; 
+
+function checkSpam(source, example){
+    if (typeof(source) !== 'string' ){
+        return console.log ('source is not a string');
+    }
+    if (typeof(example) !== 'string') {
+        return console.log ('example is not a string');
+    }
+
+    example = example.toLowerCase(); 
+    source = source.toLowerCase();
+
+    if (source.search(example) >= 0){
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+        
+};
+checkSpam('pitterXXX@gmail.com', 'xxx') // true
+checkSpam('pitterxxx@gmail.com', 'XXX') // true
