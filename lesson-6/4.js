f2b5bb04a58270625055c2065d792219c06a66b2
juricon <yuriyvladim@gmail.com>
2019-07-14 02:10:08 +0300

    Update 4.js

--- lesson-6/4.js
+++ lesson-6/4.js
@@ -10,16 +10,52 @@
 // * - В качестве второго аргумента была передана не функция.
 // */
 
-const array = [1, 2, 'Добро пожаловать.', 4, 5, 6];
+// const array = [1, 2, 'Добро пожаловать.', 4, 5, 6];
 
-// Решение
+// // Решение
+//by teacher  Dima Vakatsiienko
 
-const result = some(array, function(item, i, arrayRef) {
-   console.log(item); // элемент массива
-   console.log(i); // индекс элемента
-   console.log(arrayRef); // ссылка на обрабатываемый массив
+function some(array, callback) {
+    if (arguments.length !== 2) {
+        throw new Error('function be called with two arguments.');
+    }
 
-   return typeof item === 'string';
+    if (!Array.isArray(array)) {
+        throw new Error('first parameter type is not an Array');
+    }
+
+    if (!(typeof callback === 'function')) {
+        throw new Error('second parameter type is not a Function');
+    }
+
+    let status = false;
+
+    for (let counter = 0; counter < array.length; counter++) {
+        const current = array[counter];
+        status = callback(current, counter, array);
+
+        if (status) {
+            break;
+        }
+    }
+
+    return status;
+}
+
+const array = [1, 3, 7, 1];
+
+const result = some(array, function(item, i, array) {
+    return item % 2 === 0;
 });
 
-console.log(result); // true
+console.log(result);
+
+// const result = some(array, function(item, i, arrayRef) {
+//    console.log(item); // элемент массива
+//    console.log(i); // индекс элемента
+//    console.log(arrayRef); // ссылка на обрабатываемый массив
+
+//    return typeof item === 'string';
+// });
+
+// console.log(result); // true
