8b6b4691977f9aeb968fb6bde59117eb230c587c
juricon <yuriyvladim@gmail.com>
2019-07-28 17:44:09 +0300

    Create 1.js
    some thoughts. Here I'm trying to understand how Object.create works. Next step is going to be disassembling this method and  give an alternative

---
+++ lesson-9/1.js
@@ -0,0 +1,52 @@
+Задача 1.
+ *
+ * Создайте функцию shallowMerge.
+ * Функция обладает двумя параметрами, каждый из которых должен быть обычным JavaScript объектом.
+ * Функция возвращает новый объект, который в себе объединяет свойства обоих объектов.
+ * Свойства необходимо копировать лишь на один уровень каждого их объектов.
+ *
+ * Из объектов и обеих аргументах копируются только собственные свойства, включая недоступные для перечисления.
+ *
+ * Условия:
+ * - Встроенный метод Object.create() использовать запрещено;
+ * - При копировании любого свойства необходимо обязательно сохранить его дескрипторы;
+ * - Свойства с одинаковыми именами нужно перезаписывать — приоритетом обладает объект из второго параметра.
+ */
+
+// Решение
+ function shallowMerge(user, userData){
+//   let firstArg =  Object.create(
+//         Object.getPrototypeOf(user),
+//         Object.getOwnPropertyDescriptors(user),
+//     );
+//   let secondArg = Object.create(
+//     Object.getPrototypeOf(userData),
+//     Object.getOwnPropertyDescriptors(userData),
+//     );
+
+    
+
+// below  36-44 the same as  Object.assign()  
+let clone = {}; 
+for (let key in user) {
+  clone[key] = user[key];
+}
+for (let key in userData) {
+  clone[key] = userData[key];
+}
+
+return clone;
+}
+//
+
+const user = { firstName: 'Marcus', lastName: 'Kronenberg' };
+const userData = { job: 'developer', country: 'Germany', lastName: 'Schmidt' };
+
+Object.defineProperty(user, 'firstName', { writable: false });
+Object.defineProperty(userData, 'job', { configurable: false });
+
+const result = shallowMerge(user, userData);
+
+console.log(result); // { firstName: 'Marcus', lastName: 'Schmidt', job: 'developer', country: 'Germany' }
+console.log(Object.getOwnPropertyDescriptor(result, 'firstName').writable); // false
+console.log(Object.getOwnPropertyDescriptor(result, 'job').configurable); // false
